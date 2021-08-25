87655e0be58e66ccab122d780cf2b1ee474326ce
Brandon Sturgeon <sturgeonb4@gmail.com>
2021-08-25 12:37:49 -0500

    Feature/radical-nitrous-injection (#2)
    * Optimizations
    
    * Fix spacing
    
    * Fixes, refactors, indentation, whitespace, linebreaks
    
    * Fix limits
    
    * Undo adv2 code
    
    * Cache check lookups, fix err
    
    * Syntax fixes
    
    * Fix no cache print & removed whitespace
    
    * Fix style
    
    * Localize tsayError
    
    * Remove urs.weapons2
    
    * Fix typo
    
    * Remove unessary and checks
    
    * Fix limits
    
    * Fix a lot of style issues
    
    * Fix many style issues
    
    * More style fixes & remove unused vars
    
    * Single char style fix
    
    * Localize hook functions
    
    * Bug fix
    
    * Redo localization of hooks
    
    * Set cache on initialspawn
    
    * Only printrestricted if restricted
    
    Co-authored-by: wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>

--- lua/ulx/modules/urs_server.lua
+++ lua/ulx/modules/urs_server.lua
@@ -1,193 +1,279 @@
 AddCSLuaFile( "ulx/modules/sh/urs_cmds.lua" )
 
-if !URS then URS = {} end 
+URS = URS or {}
 
 HOOK_LOW = HOOK_LOW or 1 -- ensure we don't break on old versions of ULib
 
-function URS.Load() 
-	URS.restricions = {} 
-	URS.limits = {}
-	URS.loadouts = {} 
+function URS.Load()
+    URS.restrictions = {}
+    URS.limits = {}
 
+    if file.Exists( "ulx/restrictions.txt", "DATA" ) then URS.restrictions = util.JSONToTable( file.Read( "ulx/restrictions.txt", "DATA" ) ) end
+    if file.Exists( "ulx/limits.txt", "DATA" ) then URS.limits = util.JSONToTable( file.Read( "ulx/limits.txt", "DATA" ) ) end
 
-	if file.Exists( "ulx/restrictions.txt", "DATA" ) then URS.restrictions = util.JSONToTable( file.Read( "ulx/restrictions.txt", "DATA" ) ) end
-	if file.Exists( "ulx/limits.txt", "DATA" ) then URS.limits = util.JSONToTable( file.Read( "ulx/limits.txt", "DATA" ) ) end
-	if file.Exists( "ulx/loadouts.txt", "DATA" ) then URS.loadouts = util.JSONToTable( file.Read( "ulx/loadouts.txt", "DATA" ) ) end
+    -- Initiallize all tables to prevent errors
+    for ursType, types in pairs(URS.types) do
+        if not URS[ursType] then
+            URS[ursType] = {}
+        end
 
-	-- Initiallize all tables to prevent errors
-	for type, types in pairs(URS.types) do 
-		if !URS[type] then 
-			URS[type] = {} 
-		end 
-
-		for k, v in pairs(types) do 
-			if !URS[type][v] then 
-				URS[type][v] = {} 
-			end 
-		end 
-	end 
+        for _, v in pairs(types) do
+            if not URS[ursType][v] then
+                URS[ursType][v] = {}
+            end
+        end
+    end
 end
 
 URS_SAVE_ALL = 0
-URS_SAVE_RESTRICIONS = 1 
-URS_SAVE_LIMITS = 2 
-URS_SAVE_LOADOUTS = 3
+URS_SAVE_RESTRICTIONS = 1
+URS_SAVE_LIMITS = 2
 
 function URS.Save(n)
-	if (n == URS_SAVE_ALL or n == URS_SAVE_RESTRICTIONS) 	and URS.restrictions then file.Write("ulx/restrictions.txt", util.TableToJSON(URS.restrictions)) end
-	if (n == URS_SAVE_ALL or n == URS_SAVE_LIMITS) 			and URS.limits then file.Write("ulx/limits.txt", util.TableToJSON(URS.limits)) end
-	if (n == URS_SAVE_ALL or n == URS_SAVE_LOADOUTS) 		and URS.loadouts then file.Write("ulx/loadouts.txt", util.TableToJSON(URS.loadouts)) end
+    if (n == URS_SAVE_ALL or n == URS_SAVE_RESTRICTIONS) 	and URS.restrictions then file.Write("ulx/restrictions.txt", util.TableToJSON(URS.restrictions)) end
+    if (n == URS_SAVE_ALL or n == URS_SAVE_LIMITS) 			and URS.limits then file.Write("ulx/limits.txt", util.TableToJSON(URS.limits)) end
+
+    for _, ply in ipairs( player.GetAll() ) do
+        ply.URS_CacheCheck = nil
+    end
 end
 
-function URS.PrintRestricted(ply, type, what) 
-	if type == "pickup" then return end -- Constant spam
-	if URS.cfg.echoSpawns:GetBool() then 
-		ulx.logSpawn(ply:Nick() .."<".. ply:SteamID() .."> spawned/used ".. type .." ".. what .." -=RESTRICTED=-")
-	end 
-	ULib.tsayError(ply, "\"".. what .."\" is a restricted ".. type .." from your rank.")
-end 
+local echoSpawns
+local overwriteSbox
+local weaponPickups
 
-function URS.Check(ply, type, what)
-	what = string.lower(what) 
-	local group = ply:GetUserGroup() 
-	local restriction = false
-
-	if URS.restrictions[type] and URS.restrictions[type][what] then 
-		restriction = URS.restrictions[type][what] 
-	end 
-
-	if restriction then 
-		if table.HasValue(restriction, "*") then 
-			if !(table.HasValue(restriction, group) or table.HasValue(restriction, ply:SteamID())) then 
-				URS.PrintRestricted(ply, type, what)
-				return false
-			end 
-		elseif table.HasValue(restriction, group) or table.HasValue(restriction, ply:SteamID()) then 
-			URS.PrintRestricted(ply, type, what) 
-			return false 
-		end 
-	end 
-	
-	if URS.restrictions["all"] and URS.restrictions["all"][type] and table.HasValue(URS.restrictions["all"][type], group) then 
-		ULib.tsayError(ply, "Your rank is restricted from all ".. type .."s") 
-		return false 
-	elseif table.HasValue(URS.types.limits, type) and URS.limits[type] and (URS.limits[type][ply:SteamID()] or URS.limits[type][group]) then 
-		if URS.limits[type][ply:SteamID()] then 
-			if ply:GetCount(type.."s") >= URS.limits[type][ply:SteamID()] then 
-				ply:LimitHit( type .."s" )
-				return false 
-			end 
-		elseif URS.limits[type][group] then 
-			if ply:GetCount(type.."s") >= URS.limits[type][group] then 
-				ply:LimitHit( type .."s" )
-				return false 
-			end 
-		end 
-		if URS.cfg.overwriteSbox:GetBool() then 
-			return true -- Overwrite sbox limit (ours is greater) 
-		end 
-	end 
-end
-
-timer.Simple(0.1, function() 
-
-	--  Wiremod's Advanced Duplicator
-	if AdvDupe then 
-		AdvDupe.AdminSettings.AddEntCheckHook( "URSDupeCheck", 
-		function(ply, Ent, EntTable) 
-			return URS.Check( ply, "advdupe", EntTable.Class )
-		end, 
-		function(Hook) 
-			ULib.tsayColor( nil, false, Color( 255, 0, 0 ), "URSDupeCheck has failed.  Please contact Aaron113 @\nhttp://forums.ulyssesmod.net/index.php/topic,5269.0.html" )
-		end )
-	end
-
-	-- Advanced Duplicator 2 (http://facepunch.com/showthread.php?t=1136597)
-	if AdvDupe2 then 
-		hook.Add("PlayerSpawnEntity", "URSCheckRestrictedEntity", function(ply, EntTable) 
-			if URS.Check(ply, "advdupe", EntTable.Class) == false or URS.Check(ply, "advdupe", EntTable.Model) == false then 
-				return false 
-			end 
-		end) 
-	end 
-
+hook.Add( "Initialize", "URSConfLoad", function()
+    echoSpawns = URS.cfg.echoSpawns:GetBool()
+    overwriteSbox = URS.cfg.overwriteSbox:GetBool()
+    weaponPickups = URS.cfg.weaponPickups:GetInt()
 end )
 
-function URS.CheckRestrictedSENT(ply, sent)
-	return URS.Check( ply, "sent", sent )
+local IsValid = IsValid
+local logSpawn = ulx.logSpawn
+local tsayError = ULib.tsayError
+local stringLower = string.lower
+local rawget = rawget
+local rawset = rawset
+
+function URS.PrintRestricted(ply, restrictionType, what)
+    if restrictionType == "pickup" then return end -- Constant spam
+
+    if echoSpawns then
+        logSpawn(ply:Nick() .. "<" .. ply:SteamID() .. "> spawned/used " .. restrictionType .. " " .. what .. " -=RESTRICTED=-" )
+    end
+    tsayError(ply, "\"" .. what .. "\" is a restricted " .. restrictionType .. " from your rank." )
 end
-hook.Add( "PlayerSpawnSENT", "URSCheckRestrictedSENT", URS.CheckRestrictedSENT, HOOK_LOW )
+local PrintRestricted = URS.PrintRestricted
+
+-- Return cached value for this check
+function URS.CachedCheck(ply, restrictionType, what)
+    local existing = ply.URS_CacheCheck
+    local restrictionTypes = rawget( existing, restrictionType )
+
+    if not restrictionTypes then return end
+
+    -- true, false, or nil
+    return rawget( restrictionTypes, what )
+end
+local cachedCheck = URS.CachedCheck
+
+-- Caches the given check and returns the given result
+function URS.CacheCheck(ply, restrictionType, what, result)
+    local existing = ply.URS_CacheCheck
+    local restrictionTypes = rawget( existing, restrictionType )
+
+    if not restrictionTypes then
+        rawset( existing, restrictionType, {} )
+        return result
+    end
+
+    rawset( restrictionTypes, what, result )
+
+    return result
+end
+local cacheCheck = URS.CacheCheck
+
+function URS.Check(ply, restrictionType, what)
+    local cachedResult = cachedCheck( ply, restrictionType, what )
+    if cachedResult ~= nil then
+        if cachedResult == false then
+            PrintRestricted( ply, restrictionType, what )
+        end
+        return cachedResult
+    end
+
+    what = stringLower(what)
+    local restrictionTypePlural = restrictionType .. "s"
+    local group = ply:GetUserGroup()
+
+    local restriction = false
+
+    local ursRestrictions = rawget( URS, "restrictions" )
+    local typeRestrictions = ursRestrictions and rawget( ursRestrictions, restrictionType )
+    local theseRestrictions = typeRestrictions and rawget( typeRestrictions, what )
+
+    if theseRestrictions then
+        restriction = theseRestrictions
+    end
+
+    if restriction then
+        local hasGroup = rawget( restriction, group )
+        local hasPlayer = rawget( restriction, ply:SteamID() )
+
+        if rawget( restriction, "*" ) then
+            if not ( hasGroup or hasPlayer ) then
+                PrintRestricted( ply, restrictionType, what )
+                return cacheCheck( ply, restrictionType, what, false )
+            end
+        elseif hasGroup or hasPlayer then
+            PrintRestricted( ply, restrictionType, what )
+            return cacheCheck( ply, restrictionType, what, false )
+        end
+    end
+
+    local allRestrictions = rawget( ursRestrictions, "all" )
+    local allTypeRestrictions = allRestrictions and rawget( allRestrictions, restrictionType )
+    local hasGroup = allTypeRestrictions and rawget( allTypeRestrictions, group )
+
+    if hasGroup then
+        tsayError(ply, "Your rank is restricted from all " .. restrictionTypePlural)
+        return cacheCheck( ply, restrictionType, what, false )
+    end
+
+    local ursTypes = rawget( URS, "types" )
+    local ursTypeLimits = rawget( ursTypes, "limitsMap" )
+    local limitsHasType = rawget( ursTypeLimits, restrictionType )
+    if not limitsHasType then return end
+
+    local ursLimits = rawget( URS, "limits" )
+    local typeLimits = rawget( ursLimits, restrictionType )
+    if not typeLimits then return end
+
+    local playerTypeLimit = rawget( typeLimits, ply:SteamID() )
+    local groupTypeLimit = rawget( typeLimits, group )
+    if not playerTypeLimit and not groupTypeLimit then return end
+
+    -- TODO: Should this be an elseif? Shouldn't both cases be possible?
+    if playerTypeLimit then
+        if ply:GetCount(restrictionTypePlural) >= playerTypeLimit then
+            ply:LimitHit( restrictionTypePlural )
+            return false
+        end
+    elseif groupTypeLimit then
+        if ply:GetCount(restrictionTypePlural) >= groupTypeLimit then
+            ply:LimitHit( restrictionTypePlural )
+            return false
+        end
+    end
+    if overwriteSbox then
+        return true -- Overwrite sbox limit (ours is greater)
+    end
+end
+local Check = URS.Check
+
+timer.Simple( 0.1, function()
+    --  Wiremod's Advanced Duplicator
+    if AdvDupe then
+        AdvDupe.AdminSettings.AddEntCheckHook( "URSDupeCheck",
+        function( ply, _, EntTable )
+            return Check( ply, "advdupe", EntTable.Class )
+        end,
+        function()
+            ULib.tsayColor( nil, false, Color( 255, 0, 0 ), "URSDupeCheck has failed.  Please contact Aaron113 @\nhttp://forums.ulyssesmod.net/index.php/topic,5269.0.html" )
+        end )
+    end
+
+    -- Advanced Duplicator 2 (http://facepunch.com/showthread.php?t=1136597)
+    if AdvDupe2 then
+        hook.Add("PlayerSpawnEntity", "URSCheckRestrictedEntity", function(ply, EntTable)
+            if Check(ply, "advdupe", EntTable.Class) == false or Check(ply, "advdupe", EntTable.Model) == false then
+                return false
+            end
+        end)
+    end
+end)
+
+function URS.CheckRestrictedSENT(ply, sent)
+    return Check( ply, "sent", sent )
+end
+local CheckRestrictedSENT = URS.CheckRestrictedSENT
+hook.Add( "PlayerSpawnSENT", "URSCheckRestrictedSENT", CheckRestrictedSENT, HOOK_LOW )
 
 function URS.CheckRestrictedProp(ply, mdl)
-	return URS.Check( ply, "prop", mdl )
+    return Check( ply, "prop", mdl )
 end
-hook.Add( "PlayerSpawnProp", "URSCheckRestrictedProp", URS.CheckRestrictedProp, HOOK_LOW )
+local CheckRestrictedProp = URS.CheckRestrictedProp
+hook.Add( "PlayerSpawnProp", "URSCheckRestrictedProp", CheckRestrictedProp, HOOK_LOW )
+
+local ignoredTools = {
+    inflator = true,
+    paint = true
+}
 
 function URS.CheckRestrictedTool(ply, tr, tool)
-	if URS.Check( ply, "tool", tool ) == false then return false end
-    if not IsValid( ply ) then return end
-	if URS.cfg.echoSpawns:GetBool() and tool != "inflator" then
-		ulx.logSpawn( ply:Nick().."<".. ply:SteamID() .."> used the tool ".. tool .." on ".. tr.Entity:GetModel() )
-	end
-end
-hook.Add( "CanTool", "URSCheckRestrictedTool", URS.CheckRestrictedTool, HOOK_LOW )
+    if Check( ply, "tool", tool ) == false then return false end
 
-function URS.CheckRestrictedEffect(ply, mdl)
-	return URS.Check( ply, "effect", mdl )
-end
-hook.Add( "PlayerSpawnEffect", "URSCheckRestrictedEffect", URS.CheckRestrictedEffect, HOOK_LOW )
+    if not echoSpawns then return end
+    if ignoredTools[tool] then return end
 
-function URS.CheckRestrictedNPC(ply, npc, weapon)
-	return URS.Check( ply, "npc", npc )
-end
-hook.Add( "PlayerSpawnNPC", "URSCheckRestrictedNPC", URS.CheckRestrictedNPC, HOOK_LOW )
+    local ent = rawget( tr, "Entity" )
+    if not IsValid( ent ) then return end
 
-function URS.CheckRestrictedRagdoll(ply, mdl)
-	return URS.Check( ply, "ragdoll", mdl )
+    logSpawn( ply:Nick() .. "<" .. ply:SteamID() .. "> used the tool " .. tool .. " on " .. ent:GetModel() )
 end
-hook.Add( "PlayerSpawnRagdoll", "URSCheckRestrictedRagdoll", URS.CheckRestrictedRagdoll, HOOK_LOW )
+local CheckRestrictedTool = URS.CheckRestrictedTool
+hook.Add( "CanTool", "URSCheckRestrictedTool", CheckRestrictedTool, HOOK_LOW )
 
-function URS.CheckRestrictedSWEP(ply, class, weapon)
-	if URS.Check( ply, "swep", class ) == false then 
-		return false 
-	elseif URS.cfg.echoSpawns:GetBool() then 
-		ulx.logSpawn( ply:Nick().."<".. ply:SteamID() .."> spawned/gave himself swep ".. class ) 
-	end 
+function URS.CheckRestrictedEffect( ply, mdl )
+    return Check( ply, "effect", mdl )
 end
-hook.Add( "PlayerSpawnSWEP", "URSCheckRestrictedSWEP", URS.CheckRestrictedSWEP, HOOK_LOW )
-hook.Add( "PlayerGiveSWEP", "URSCheckRestrictedSWEP2", URS.CheckRestrictedSWEP, HOOK_LOW )
+local CheckRestrictedEffect = URS.CheckRestrictedEffect
+hook.Add( "PlayerSpawnEffect", "URSCheckRestrictedEffect", CheckRestrictedEffect, HOOK_LOW )
 
-function URS.CheckRestrictedPickUp(ply, weapon)
-	if URS.cfg.weaponPickups:GetInt() == 2 then
-		if URS.Check( ply, "pickup", weapon:GetClass(), true) == false then 
-			return false 
-		end 
-	elseif URS.cfg.weaponPickups:GetInt() == 1 then
-		if URS.Check( ply, "swep", weapon:GetClass()) == false then 
-			return false 
-		end 
-	end
+function URS.CheckRestrictedNPC( ply, npc )
+    return Check( ply, "npc", npc )
 end
-hook.Add( "PlayerCanPickupWeapon", "URSCheckRestrictedPickUp", URS.CheckRestrictedPickUp, HOOK_LOW )
+local CheckRestrictedNPC = URS.CheckRestrictedNPC
+hook.Add( "PlayerSpawnNPC", "URSCheckRestrictedNPC", CheckRestrictedNPC, HOOK_LOW )
 
-function URS.CheckRestrictedVehicle(ply, mdl, name, vehicle_table)
-	return URS.Check( ply, "vehicle", mdl ) and URS.Check( ply, "vehicle", name )
+function URS.CheckRestrictedRagdoll( ply, mdl )
+    return Check( ply, "ragdoll", mdl )
 end
-hook.Add( "PlayerSpawnVehicle", "URSCheckRestrictedVehicle", URS.CheckRestrictedVehicle, HOOK_LOW )
+local CheckRestrictedRagdoll = URS.CheckRestrictedRagdoll
+hook.Add( "PlayerSpawnRagdoll", "URSCheckRestrictedRagdoll", CheckRestrictedRagdoll, HOOK_LOW )
 
-function URS.CustomLoadouts(ply)
-	if URS.loadouts[ply:SteamID()] then
-		ply:StripWeapons()
-		for k, v in pairs( URS.loadouts[ply:SteamID()] ) do
-			ply:Give( v )
-		end
-		return true
-	elseif URS.loadouts[ply:GetUserGroup()] then
-		ply:StripWeapons()
-		for k, v in pairs( URS.loadouts[ply:GetUserGroup()] ) do
-			ply:Give( v )
-		end
-		return true
-	end
+function URS.CheckRestrictedSWEP( ply, class )
+    if Check( ply, "swep", class ) == false then
+        return false
+    end
+
+    if not echoSpawns then return end
+
+    logSpawn( ply:Nick() .. "<" .. ply:SteamID() .. "> spawned/gave himself swep " .. class )
 end
-hook.Add( "PlayerLoadout", "URSCustomLoadouts", URS.CustomLoadouts, HOOK_LOW )
+local CheckRestrictedSWEP = URS.CheckRestrictedSWEP
+hook.Add( "PlayerSpawnSWEP", "URSCheckRestrictedSWEP", CheckRestrictedSWEP, HOOK_LOW )
+hook.Add( "PlayerGiveSWEP", "URSCheckRestrictedSWEP2", CheckRestrictedSWEP, HOOK_LOW )
+
+function URS.CheckRestrictedPickUp( ply, weapon )
+    if weaponPickups == 2 and Check( ply, "pickup", weapon:GetClass(), true ) == false then
+        return false
+    end
+
+    if weaponPickups == 1 and Check( ply, "swep", weapon:GetClass() ) == false then
+        return false
+    end
+end
+local CheckRestrictedPickUp = URS.CheckRestrictedPickUp
+hook.Add( "PlayerCanPickupWeapon", "URSCheckRestrictedPickUp", CheckRestrictedPickUp, HOOK_LOW )
+
+function URS.CheckRestrictedVehicle( ply, mdl, name )
+    return Check( ply, "vehicle", mdl ) and Check( ply, "vehicle", name )
+end
+local CheckRestrictedVehicle = URS.CheckRestrictedVehicle
+hook.Add( "PlayerSpawnVehicle", "URSCheckRestrictedVehicle", CheckRestrictedVehicle, HOOK_LOW )
+
+hook.Add( "PlayerInitialSpawn", "URSSetCheckCache", function( ply )
+    ply.URS_CacheCheck = {}
+end )
+
